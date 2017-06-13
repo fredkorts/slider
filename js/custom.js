6ed71321ef2f3152e57f3c6581e8d8d75092ac0a
Fred Korts <fredkorts@gmail.com>
2017-06-13 13:57:09 +0300

    Slider test

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+var playing = true;
+var next = document.getElementById('left');
+var previous = document.getElementById('right');
+
+function pauseSlideshow() {
+    playing = false;
+    clearInterval(slideInterval);
+}
+function playSlideshow() {
+    playing = true;
+    slideInterval = setInterval(nextSlide,2000);
+}
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+function nextSlide() {
+    goToSlide(currentSlide+1);
+}
+
+function previousSlide() {
+    goToSlide(currentSlide-1);
+}
+
+function goToSlide(n) {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+next.onclick = function() {
+	pauseSlideshow();
+    nextSlide();
+};
+previous.onclick = function() {
+	pauseSlideshow();
+    previousSlide();
+};
